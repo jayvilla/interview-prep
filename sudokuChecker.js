5d397d13cace080bc1eb189868f08c708529dd8d
Jeff Villarina <jeffvilla@Jeffreys-MacBook-Pro.local>
2017-05-02 16:11:20 -0700

    (Complete) Refactor sudokuChecker solution

--- sudokuChecker.js
+++ sudokuChecker.js
@@ -1,9 +1,62 @@
-// I - 9x9 board
-// O - boolean
-// C - ???
-// E - ???
+const sudokuChecker = board => {
+  const rowCheck = (rowIndex, validRows) => {
+    for (let number of board[rowIndex]) {
+      if (!validRows[number]) return false;
+      validRows[number] --;
+    }
+    return true;
+  }
 
-let example = [
+ const columnCheck = (colIndex, validRows) => {
+    let current;
+    for (let i = 0; i < board.length; i ++) {
+      current = board[i][colIndex];
+      if (!validRows[current]) return false;
+      validRows[current] --;
+    }
+    return true;
+  }
+
+ const boxCheck = (x, y, validRows) => {
+    let current;
+    for (let i = 0; i < 3; i ++) {
+      for (let j = 0; j < 3; j ++) {
+        current = board[x + i][y + j];
+        if (!validRows[current]) return false;
+        validRows[current] --;
+      }
+    }
+    return true;
+  }
+
+ const resetMoves = (obj) => {
+    return {
+    '1': 1,
+    '2': 1,
+    '3': 1,
+    '4': 1,
+    '5': 1,
+    '6': 1,
+    '7': 1,
+    '8': 1,
+    '9': 1
+    };
+  }
+
+ for (let i = 0; i < board.length; i ++) {
+    if (!rowCheck(i, resetMoves())) return false;
+    if (!columnCheck(i, resetMoves())) return false;
+  }
+
+ for (i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j += 3) {
+      if (!boxCheck(i, j, resetMoves())) return false;
+    }
+  }
+  return true;
+}
+
+let board = [
   [5,3,4,6,7,8,9,1,2],
   [6,7,2,1,9,5,3,4,8],
   [1,9,8,3,4,2,5,6,7],
@@ -14,35 +67,4 @@ let example = [
   [2,8,7,4,1,9,6,3,5],
   [3,4,5,2,8,6,1,7,9]
 ];
-
-// ['.',4,'.', ]
-// Check horizontals for duplicates
-// Check verticals for duplicates
-
-const sudokuChecker = board => {
-  let horiz = [], vert = [];
-
-  for (let i = 0; i < board.length; i++) {
-    horiz = [];
-    vert = [];
-
-    for (let j = 0; j < board.length; j++) {
-      if (horiz.includes(board[i][j]) || vert.includes(board[j][i])) {
-        return false;
-      }
-
-      vert.push(board[j][i]);
-      horiz.push(board[i][j]);
-    }
-  }
-
-  for (let i = 0; i < 9; i += 3) {
-    for (let j = 0; j < 9; j += 3) {
-      /* Insert code here */
-    }
-  }
-  return true;
-};
-
-console.log(sudokuChecker(example));
-
+console.log(sudokuChecker(board))
