855dc2e174ffd8f628f2a4712cfe668498893e0d
Jeff Villarina <villarina.jeffrey@gmail.com>
2017-05-09 15:39:33 -0700

    (Incomplete) Need to complete makingChange.js(Incomplete) Need to complete makingChange.js(Incomplete) Need to complete makingChange.js(Incomplete) Need to complete makingChange.js(Incomplete) Need to complete makingChange.js(Incomplete) Need to complete makingChange.js(Incomplete) Need to complete makingChange.js(Incomplete) Need to complete makingChange.js(Incomplete) Need to complete makingChange.js

---
+++ makingChange.js
@@ -0,0 +1,28 @@
+/* Imagine you landed a new job as a cashier...
+
+YOur quirky boss foudn out that you're a programmer and has a weird request about
+something they've been wondering for a long time.
+
+Write a function that, given:
+  1. an amount of money
+  2. a list of coin denominations
+
+computes the number of ways to make amount of money with coins of the available
+denominations
+
+Example: for amount=4(4c) and denominations=[1, 2, 3](1C, 2C, and 3C) your program
+would output 4-the number of ways to make 4c with those denominations
+
+  1. 1c, 1c, 1c, 1c
+  2. 1c, 1c, 2c
+  3. 1c, 3c
+  4. 2c, 2c */
+
+  const makingChanges = (amount, denominations) => {
+    /* TODO */
+  };
+
+  const exampleAmount = 4;
+  const exampleDenominations = [1, 2, 3];
+
+  console.log(makingChanges(exampleAmount, exampleDenominations));
